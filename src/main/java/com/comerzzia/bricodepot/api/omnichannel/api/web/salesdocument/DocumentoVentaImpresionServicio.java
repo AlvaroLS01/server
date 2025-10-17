c6adf366c9e1ceb9fb6d2c785f174d7883cd5872
AlvaroLS01 <136437203+AlvaroLS01@users.noreply.github.com>
2025-10-17 13:12:19 +0200

    Revert "Fix UTF-8 filename encoding for downloads"

--- src/main/java/com/comerzzia/bricodepot/api/omnichannel/api/web/salesdocument/DocumentoVentaImpresionServicio.java
+++ src/main/java/com/comerzzia/bricodepot/api/omnichannel/api/web/salesdocument/DocumentoVentaImpresionServicio.java
@@ -1,6 +1,7 @@
 package com.comerzzia.bricodepot.api.omnichannel.api.web.salesdocument;
 
 import java.io.ByteArrayOutputStream;
+import java.util.Base64;
 import java.util.Map;
 import java.util.Optional;
 
@@ -32,7 +33,7 @@ public class DocumentoVentaImpresionServicio {
                 this.datosSesionRequest = datosSesionRequest;
         }
 
-        public Optional<DocumentoVentaImpresionResultado> imprimir(String uidDocumento, OpcionesImpresionDocumentoVenta opciones) {
+        public Optional<DocumentoVentaImpresionRespuesta> imprimir(String uidDocumento, OpcionesImpresionDocumentoVenta opciones) {
 
                 if (StringUtils.isBlank(uidDocumento)) {
                         throw new DocumentoVentaImpresionException("El identificador del documento es obligatorio");
@@ -47,7 +48,7 @@ public class DocumentoVentaImpresionServicio {
                                 LOGGER.debug("imprimir() - La impresión del documento '{}' no generó contenido", uidDocumento);
                                 return Optional.empty();
                         }
-                        DocumentoVentaImpresionResultado respuesta = construirRespuesta(uidDocumento, opciones, configuracionImpresion, contenido);
+                        DocumentoVentaImpresionRespuesta respuesta = construirRespuesta(uidDocumento, opciones, configuracionImpresion, contenido);
                         return Optional.of(respuesta);
                 }
                 catch (NotFoundException excepcion) {
@@ -90,12 +91,16 @@ public class DocumentoVentaImpresionServicio {
                 return printRequest;
         }
 
-        private DocumentoVentaImpresionResultado construirRespuesta(String uidDocumento, OpcionesImpresionDocumentoVenta opciones,
+        private DocumentoVentaImpresionRespuesta construirRespuesta(String uidDocumento, OpcionesImpresionDocumentoVenta opciones,
                         PrintDocumentDTO configuracionImpresion, byte[] contenido) {
-                String mimeType = configuracionImpresion.getMimeType();
-                String nombreArchivo = normalizarNombreArchivo(configuracionImpresion.getOutputDocumentName(), mimeType);
-                return new DocumentoVentaImpresionResultado(uidDocumento, opciones.esCopia(), opciones.esEnLinea(), mimeType,
-                                nombreArchivo, contenido);
+                DocumentoVentaImpresionRespuesta respuesta = new DocumentoVentaImpresionRespuesta();
+                respuesta.setUidDocumento(uidDocumento);
+                respuesta.setCopia(opciones.esCopia());
+                respuesta.setEnLinea(opciones.esEnLinea());
+                respuesta.setTipoMime(configuracionImpresion.getMimeType());
+                respuesta.setNombreArchivo(normalizarNombreArchivo(configuracionImpresion.getOutputDocumentName(), configuracionImpresion.getMimeType()));
+                respuesta.setDocumento(Base64.getEncoder().encodeToString(contenido));
+                return respuesta;
         }
 
         private String normalizarNombreArchivo(String nombreSolicitado, String mimeType) {
