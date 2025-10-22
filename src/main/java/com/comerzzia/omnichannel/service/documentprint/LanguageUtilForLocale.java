7e8ae496254041ead6e81cc89ef44ec8d41e2afb
AlvaroLS01 <136437203+AlvaroLS01@users.noreply.github.com>
2025-10-22 12:28:16 +0200

    Restore Jasper print support for sales document PDFs

---
+++ src/main/java/com/comerzzia/omnichannel/service/documentprint/LanguageUtilForLocale.java
@@ -0,0 +1,29 @@
+package com.comerzzia.omnichannel.service.documentprint;
+
+import java.util.Locale;
+
+import com.comerzzia.core.servicios.ContextHolder;
+
+public class LanguageUtilForLocale {
+
+    private final Locale locale;
+    private final LanguageUtil languageUtil;
+
+    public LanguageUtilForLocale(Locale locale) {
+        this.locale = locale;
+        this.languageUtil = getLanguageUtilInstance();
+    }
+
+    protected LanguageUtil getLanguageUtilInstance() {
+        try {
+            return (LanguageUtil) ContextHolder.getBean("languageUtil");
+        }
+        catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String get(String key) {
+        return languageUtil.getString(locale, key);
+    }
+}
